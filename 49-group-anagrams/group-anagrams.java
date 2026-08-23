3accd737516166904e5f73a165657d51c9992492
Sumit-Bardolai <168703823+Sumit-Bardolai@users.noreply.github.com>
2026-08-23 09:18:08 +0530

    Time: 6 ms (99.33%) | Memory: 49.8 MB (44.62%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,ArrayList<String>> anagrams=new HashMap<>();
+            for(String s:strs){
+                char []c=s.toCharArray();
+              Arrays.sort(c);
+             String key=String.valueOf(c);
+             if(!anagrams.containsKey(key)){
+                anagrams.put(key, new ArrayList<>());
+             }
+             anagrams.get(key).add(s);
+            }
+            return new ArrayList<>(anagrams.values());
+
+    }
+}
